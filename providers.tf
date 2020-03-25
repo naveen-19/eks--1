e327cffebf463d199aaf4b02c49a6d81b4617491
naveen-19 <55230320+naveen-19@users.noreply.github.com>
2020-03-25 14:28:13 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -4,8 +4,9 @@
 
 provider "aws" {
   region  = "ap-south-1"
-  
-   version = ">= 2.38.0"
+  shared_credentials_file = "~/.aws/credentials"
+  profile                 = "test"
+   version = "~> 2.38.0"
   }
 
 # Using these data sources allows the configuration to be
